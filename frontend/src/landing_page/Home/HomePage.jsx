5e4df8ed12a96972e555832ceea0d7698877cdab
Prit1011 <pritgandhi0902@gmail.com>
2025-07-23 19:54:13 +0530

    changes

---
+++ frontend/src/landing_page/Home/HomePage.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+// import Hero from "./Hero";
+// import Stats from "./Stats";
+// import Pricing from "./Pricing";
+// import Education from "./Education";
+// import Awords from "./Awords";
+
+// import OpenAccount from "../OpenAccount";
+import Navbar from "../Navbar";
+// import Footer from "../Footer";
+
+function HomePage() {
+  return (
+    <>
+      <Navbar />
+      {/* <Hero />
+      <Awords />
+      <Stats />
+      <Pricing />
+      <Education />
+      <OpenAccount />
+      <Footer /> */}
+    </>
+  );
+}
+
+export default HomePage;
